83fefad8897acd764f8818206a7ed20e87e8fdd3
Admin <Admin@LAPTOP-4HRM131V>
2025-06-22 14:08:49 +0300

    working start

--- WpfLib/Converters/ReverseBoolConverter.cs
+++ WpfLib/Converters/ReverseBoolConverter.cs
@@ -4,16 +4,22 @@ using System.Windows.Data;
 
 namespace WpfLib.Converters
 {
-    internal class ReverseBoolConverter: IValueConverter
+    public class ReverseBoolConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return !(bool)value;
+            if (value is bool b)
+                return !b;
+
+            return Binding.DoNothing;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return !(bool)value;
+            if (value is bool b)
+                return !b;
+
+            return Binding.DoNothing;
         }
     }
 }
